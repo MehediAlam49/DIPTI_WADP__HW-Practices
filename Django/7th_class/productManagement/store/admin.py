0a088eedd703f0ff7fced7ea27d9e2feb9c11758
MehediAlam49 <mehedialam806@gmail.com>
2025-05-30 11:16:58 +0600

    create model and create admin

--- Django/7th_class/productManagement/store/admin.py
+++ Django/7th_class/productManagement/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from store.models import *
 
 # Register your models here.
+
+admin.site.register(booksModel)
+admin.site.register(userModel)
+admin.site.register(studentModel)
--- Django/7th_class/productManagement/store/models.py
+++ Django/7th_class/productManagement/store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class booksModel(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    published_date = models.DateField()
+
+class userModel(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    phone = models.IntegerField()
+
+class studentModel(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    semester = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone = models.IntegerField()
+    
