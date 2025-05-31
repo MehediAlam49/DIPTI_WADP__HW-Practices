b96303b7b54cc65ab9b1c2bcc5062372e0586fbb
MehediAlam49 <mehedialam806@gmail.com>
2025-05-31 23:01:49 +0600

    complete create and read system in this project

--- Django/8th_class/crud_1/crud_1/views.py
+++ Django/8th_class/crud_1/crud_1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from library.models import *
 
 def home(request):
@@ -10,6 +10,25 @@ def studentList(request):
     context = {'studentData': studentModel.objects.all()}
     return render(request, 'studentList.html',context)
 def addBook(request):
+    if request.method == 'POST':
+        book = bookModel(
+            title = request.POST.get('title'),
+            author = request.POST.get('author'),
+            date = request.POST.get('date'),
+            price = request.POST.get('price'),
+            stock = request.POST.get('stock'),
+        )
+        book.save()
+        return redirect("book")
     return render(request, 'addBook.html')
 def addStudent(request):
+    if request.method == 'POST':
+        student = studentModel(
+        name = request.POST.get('name'),
+        department = request.POST.get('department'),
+        semester = request.POST.get('semester'),
+        date = request.POST.get('date'),
+        )
+        student.save()
+        return redirect('studentList')
     return render(request, 'addStudent.html')
