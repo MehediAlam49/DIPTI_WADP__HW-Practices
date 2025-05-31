8111be7df9823f1e1565349c25bfd0beb59a64f5
MehediAlam49 <mehedialam806@gmail.com>
2025-05-31 21:00:27 +0600

    complete front end

--- Django/8th_class/crud_1/crud_1/settings.py
+++ Django/8th_class/crud_1/crud_1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'crud_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
