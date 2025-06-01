68e7270b12886cbe4cbf8272385aa25e6ee7ff16
MehediAlam49 <mehedialam806@gmail.com>
2025-06-02 00:00:57 +0600

    create and read data complete

--- Django/9th_class/school_management/inventory/models.py
+++ Django/9th_class/school_management/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class courseModel(models.Model):
+    name =models.CharField(max_length=100)
+    credits = models.IntegerField()
+    instructor = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+class studentModel(models.Model):
+    name =models.CharField(max_length=100)
+    class_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+class teacherModel(models.Model):
+    name =models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
--- Django/9th_class/school_management/school_management/views.py
+++ Django/9th_class/school_management/school_management/views.py
@@ -5,22 +5,52 @@ def home(request):
     return render(request, 'home.html')
 
 def addCourse(request):
+    if request.method == 'POST':
+        course = courseModel(
+            name = request.POST.get('name'),
+            credits = request.POST.get('credits'),
+            instructor = request.POST.get('instructor'),
+            level = request.POST.get('level'),
+        )
+        course.save()
+        return redirect('courseList')
     return render(request, 'addCourse.html')
 def editCourse(request):
     return render(request, 'editCourse.html')
 def courseList(request):
-    return render(request, 'courseList.html')
+    query = {'courseData': courseModel.objects.all()}
+    return render(request, 'courseList.html',query)
 
 def addStudent(request):
+    if request.method == 'POST':
+        student = studentModel(
+            name = request.POST.get('name'),
+            class_name = request.POST.get('class_name'),
+            gender = request.POST.get('gender'),
+            address = request.POST.get('address'),
+        )
+        student.save()
+        return redirect('studentList')
     return render(request, 'addStudent.html')
 def editStudent(request):
     return render(request, 'editStudent.html')
 def studentList(request):
-    return render(request, 'studentList.html')
+    query ={'studentData': studentModel.objects.all()}
+    return render(request, 'studentList.html',query)
 
 def addTeacher(request):
+    if request.method == 'POST':
+        teacher = teacherModel(
+            name = request.POST.get('name'),
+            designation = request.POST.get('designation'),
+            gender = request.POST.get('gender'),
+            address = request.POST.get('address'),
+        )
+        teacher.save()
+        return redirect('teacherList')
     return render(request, 'addTeacher.html')
 def editTeacher(request):
     return render(request, 'editTeacher.html')
 def teacherList(request):
-    return render(request, 'teacherList.html')
+    query = {'teacherData': teacherModel.objects.all()}
+    return render(request, 'teacherList.html',query)
