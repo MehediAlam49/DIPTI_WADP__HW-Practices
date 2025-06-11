d6a3a25317466b0a810cd85647025482c3f34979
MehediAlam49 <mehedialam806@gmail.com>
2025-06-11 23:22:09 +0600

    create edit course in course list

---
+++ Django/11th_class/school_management/school_management/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path
+from school_management.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    
+    path('addCourse/', addCourse, name='addCourse'),
+    path('editCourse/<str:myid>', editCourse, name='editCourse'),
+    path('deleteCourse/<str:myid>', deleteCourse, name='deleteCourse'),
+    path('courseList/', courseList, name='courseList'),
+
+    path('addStudent/', addStudent, name='addStudent'),
+    path('studentList/', studentList, name='studentList'),
+    path('editStudent/', editStudent, name='editStudent'),
+
+    path('addTeacher/', addTeacher, name='addTeacher'),
+    path('editTeacher/', editTeacher, name='editTeacher'),
+    path('teacherList/', teacherList, name='teacherList'),
+]
