352abc6f52746de59a1ef011a0cf06649a28382a
MehediAlam49 <mehedialam806@gmail.com>
2025-06-15 22:34:27 +0600

    create front end and register function

--- Django/14th_class/authentication/user_auth/models.py
+++ Django/14th_class/authentication/user_auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class userAuthModel(models.Model):
+    fullname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    contact = models.CharField(max_length=14)
+    password = models.CharField(max_length=20)
--- Django/14th_class/authentication/user_auth/views.py
+++ Django/14th_class/authentication/user_auth/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from user_auth.models import *
 
 # Create your views here.
 def home(request):
@@ -6,4 +7,23 @@ def home(request):
 def register(request):
     return render(request, 'register.html')
 def login(request):
+    if request.method =="POST":
+        fullname = request.POST.get('fullname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        contact = request.POST.get('contact')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password == confirm_password:
+            userInfo = userAuthModel(
+                fullname = fullname,
+                username = username,
+                email = email,
+                contact = contact,
+            )
+            userInfo.save()
+            return redirect('login')
+        else:
+            print("-------password doesn't match")
     return render(request, 'login.html')
