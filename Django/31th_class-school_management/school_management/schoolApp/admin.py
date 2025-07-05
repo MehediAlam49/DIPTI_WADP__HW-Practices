b772adedb8928ebb3246894d118a8444bb7dcdeb
MehediAlam49 <mehedialam806@gmail.com>
2025-07-05 17:38:01 +0600

    feat: Implement custom user model and update app settings for school management system

--- Django/31th_class-school_management/school_management/schoolApp/admin.py
+++ Django/31th_class-school_management/school_management/schoolApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+from schoolApp.models import *
 
 # Register your models here.
+admin.site.register(CustomUserModel)
+admin.site.register(StudentModel)
+admin.site.register(TeacherModel)
--- Django/31th_class-school_management/school_management/schoolApp/models.py
+++ Django/31th_class-school_management/school_management/schoolApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUserModel(AbstractUser):
+    USER_TYPE = [
+        ('Admin', 'Admin'),
+        ('Teacher', 'Teacher'),
+    ]
+
+    user_type = models.CharField(choices=USER_TYPE, max_length=100, null=True)
+
+class TeacherModel(models.Model):
+    user = models.OneToOneField(CustomUserModel, on_delete=models.CASCADE, null=True)
+    subject = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=18, null=True)
+    hire_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+class StudentModel(models.Model):
+    teacher = models.ForeignKey(TeacherModel, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(CustomUserModel, on_delete=models.CASCADE, null=True)
+    age = models.PositiveIntegerField(null=True)
+    grade = models.CharField(max_length=20,null=True)
+    enrollment_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
