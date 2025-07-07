236545d07da7e33be28e13bda3c1a96fb07cd418
MehediAlam49 <mehedialam806@gmail.com>
2025-07-07 21:43:47 +0600

    feat: Implement student management features including add, edit, and view functionalities with profile image support

--- Django/32th_class/event_management_system/booking/views.py
+++ Django/32th_class/event_management_system/booking/views.py
@@ -2,11 +2,39 @@ from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth import authenticate,login,logout,update_session_auth_hash,hashers
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
+from booking.models import *
 
 # Create your views here.
 def signupPage(request):
+    if request.method == 'POST':
+        profile_image = request.FILES.get('profile_image')
+        username = request.POST.get('username')
+        full_name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password == confirm_password:
+            EventUserModel.objects.create_user(
+                profile_image=profile_image,
+                username=username,
+                full_name=full_name,
+                email=email,
+                phone_number=phone_number,
+                password=confirm_password,
+            )
+            return redirect('loginPage')
     return render(request, 'signupPage.html')
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
     return render(request, 'loginPage.html')
 def logoutPage(request):
     return redirect('loginPage')
