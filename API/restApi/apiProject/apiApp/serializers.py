f1184c3b46f58835edd3d6c96463759c57d75eea
MehediAlam49 <mehedialam806@gmail.com>
2025-07-29 21:47:30 +0600

    feat: Enhance StudentSerializer to manage student roll numbers and usernames during creation

--- API/restApi/apiProject/apiApp/serializers.py
+++ API/restApi/apiProject/apiApp/serializers.py
@@ -5,3 +5,24 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentModel
         fields = '__all__'
+
+
+    # def create(self, validated_data):
+    #     instance = StudentModel.objects.create(**validated_data)
+    #     instance.student_roll = 2
+    #     instance.save()
+    #     return instance
+
+    def create(self, validated_data):
+        name = validated_data.get('student_name')
+        student = StudentModel.objects.order_by('-student_roll').first()
+        if student:
+            roll = student.student_roll + 1
+        else:
+            roll = 1
+
+        student_data = StudentModel.objects.create(**validated_data)
+        student_data.student_roll = roll
+        student_data.username = (name + str(roll)).lower()
+        student_data.save()
+        return student_data
