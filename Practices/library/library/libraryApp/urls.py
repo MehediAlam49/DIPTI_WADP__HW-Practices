30820bcafd6243ae10875a72d76dbddbde4fa6ad
MehediAlam49 <mehedialam806@gmail.com>
2025-08-19 23:35:23 +0600

    add book management features: create, list, detail, update, and delete views with corresponding templates

--- Practices/library/library/libraryApp/urls.py
+++ Practices/library/library/libraryApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from libraryApp.views import *
 urlpatterns = [
-    path('', book_create, name='book_create')
+    path('', book_create, name='book_create'),
+    path('book_list/', book_list, name='book_list'),
+    path('book_detail/<str:pk>/', book_detail, name='book_detail'),
+    path('book_update/<str:pk>/', book_update, name='book_update'),
+    path('book_delete/<str:pk>/', book_delete, name='book_delete'),
 ]
--- Practices/library/library/libraryApp/views.py
+++ Practices/library/library/libraryApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from libraryApp.models import *
 from libraryApp.forms import *
 
@@ -14,4 +14,24 @@ def book_create(request):
     return render(request, 'book_create.html',{'form':form})
 
 def book_list(request):
-    pass
+    books = BookModel.objects.all()
+    return render(request, 'book_list.html',{'books':books})
+
+def book_detail(request,pk):
+    book = get_object_or_404(BookModel,pk=pk)
+    return render(request, 'book_detail.html',{'book':book})
+
+def book_update(request,pk):
+    book = get_object_or_404(BookModel,pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book_update.html',{'form':form})
+
+def book_delete(request,pk):
+    get_object_or_404(BookModel,pk=pk).delete()
+    return redirect('book_list')
